c709813248dac105cc70b97136c716466f1e4b63
Ankit <ankitmittal0814@gmail.com>
2025-08-11 16:50:54 +0530

    Update in constants

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,5 @@
 // const API_URL = 'https://dev-api.oshoppe.in/api';
+
 const API_URL = 'http://localhost:3000/api';
 
 const IMAGE_URL = 'https://paintplus.s3.ap-south-1.amazonaws.com/development/';
