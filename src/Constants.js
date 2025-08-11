a121e6480a155c2d0103e798fe21af73a2608e4a
Ankit <ankitmittal0814@gmail.com>
2025-08-11 16:52:57 +0530

    Add comments on api url

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,8 @@
+//Old API URL development
 // const API_URL = 'https://dev-api.oshoppe.in/api';
 
+
+// New API URL local
 const API_URL = 'http://localhost:3000/api';
 
 const IMAGE_URL = 'https://paintplus.s3.ap-south-1.amazonaws.com/development/';
